aad09c83dc7dcf27884053ca9da72f67b6e47f43
Marcelo Ceolin <fxsobr@gmail.com>
2022-06-09 21:27:42 -0300

    inicializado microservice de frontend

---
+++ AspNetShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetShop.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
