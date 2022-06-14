50eb77e62a059a88bef36e7599c3e97bd12d35ab
Marcelo Ceolin <fxsobr@gmail.com>
2022-06-14 20:28:50 -0300

    criado interface de produto no frontend

---
+++ AspNetShop.Web/Models/Services/IServices/IProductService.cs
@@ -0,0 +1,10 @@
+namespace AspNetShop.Web.Models.Services.IServices;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductModel>> FindAllProducts();
+    Task<ProductModel> FindProductById(long id);
+    Task<ProductModel> CreateProduct(ProductModel productModel);
+    Task<ProductModel> UpdateProduct(ProductModel productModel);
+    Task<bool> DeleteProductById(long id);
+}
