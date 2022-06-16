af74e071dee29bf7c5175478cb24ceba4582244e
Marcelo Ceolin <fxsobr@gmail.com>
2022-06-16 13:32:47 -0300

    criado controller de produtos

---
+++ AspNetShop.Web/Controllers/Product.cs
@@ -0,0 +1,21 @@
+using AspNetShop.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetShop.Web.Controllers;
+
+public class Product: Controller
+{
+    private readonly IProductService _productService;
+
+    public Product(IProductService productService)
+    {
+        _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+    }
+
+    public async Task<IActionResult> ProductIndex()
+    {
+        var products = _productService.FindAllProducts();
+        return View(products);
+    }
+    
+}
